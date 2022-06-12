6ce49435da2370b94de14150857ba53c7aac6294
Momosa123 <mouhamadoul.sarr@gmail.com>
2022-06-12 05:12:34 +0200

    tomatoe counter app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, {useState} from "react"
 import './App.css';
 
 function App() {
+  const [tomatoes, setTomatoes] = useState(0)
+  let emoji = ""
+  for (let i = 0; i < tomatoes; i++){
+    emoji += "🍅"
+  }
+  function setPositiveTomatoe(){
+    if(tomatoes >0){
+      setTomatoes(tomatoe=>tomatoe-1)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="window">
+      <h2>Tomato Counter</h2>
+        <div className="tomato-box">
+          <h1>{emoji}</h1>
+        </div>
+          <div className="buttons-container">
+            <button className="button" onClick={()=>setTomatoes(tomatoe=>tomatoe+1)}>
+                Add 
+            </button>
+            <button className="button" onClick={()=>setPositiveTomatoe()}>
+                Remove 
+              </button>
+          </div>
+        
+      </div>
     </div>
   );
 }
