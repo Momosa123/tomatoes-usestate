c7181ff508ffb371ab2d0e5b623494e66005b03e
Momosa123 <mouhamadoul.sarr@gmail.com>
2022-06-12 23:11:36 +0200

    adding days to tomatoe counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React, {useState} from "react"
+import React, {useState, useRef} from "react"
 import './App.css';
 import Button from "./Button";
 import Tomatoe from "./Tomatoe";
 
 function App() {
-  const [tomatoes, setTomatoes] = useState(0)
-  let tomatoemoji = ""
-  for (let i = 0; i < tomatoes; i++){
-    tomatoemoji += "🍅"
-  }
-  function setPositiveTomatoe(){
-    if(tomatoes >0){
-      setTomatoes(tomatoe=>tomatoe-1)
+  const days = ["M", "T", "W", "Th", "F", "Sa", "Su"];
+  const [selectedDay, setSelectedDay] = useState("")
+  const [allTomatoes, setAllTomatoes] = useState({})
+  const boldStyling = {color:"blue"}
+
+  function addTomatoes (){
+    const newAllTomatoes = {...allTomatoes}
+    const currentDayTomatoes = newAllTomatoes[selectedDay]
+    if(currentDayTomatoes){
+      newAllTomatoes[selectedDay] = currentDayTomatoes + '🍅'
+    }else{
+      newAllTomatoes[selectedDay] = '🍅'
     }
+   setAllTomatoes(newAllTomatoes)
+}
+
+function removeTomatoes (){
+  const newAllTomatoes = {...allTomatoes}
+  const currentDayTomatoes = newAllTomatoes[selectedDay]
+  if(currentDayTomatoes){
+    
+    newAllTomatoes[selectedDay] = currentDayTomatoes.slice(1,-2) 
   }
+ setAllTomatoes(newAllTomatoes)
+}
+
+  const tomatoePerDay = days.map((day) => (
+    <div key={day} className="tomato-box">
+      <h3 onClick={()=>setSelectedDay(day)} style={selectedDay === day ? boldStyling : {}} >{day}</h3>
+      {console.log(selectedDay, allTomatoes)}
+      <Tomatoe 
+      className="tomato-day-box"
+      tomatoemoji={allTomatoes[day]}
+      />
+    </div>
+  ))
+
+ 
   return (
     <div className="App">
       <div className="window">
+      
       <h2>Tomato Counter</h2>
-      <Tomatoe tomatoemoji={tomatoemoji}/>
-       <Button setPositiveTomatoe={setPositiveTomatoe} setTomatoes={setTomatoes}/>   
+      {tomatoePerDay}
+      
+       <Button addTomatoes={addTomatoes} removeTomatoes={removeTomatoes}/>   
         
       </div>
     </div>
--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-export default function Button ({setTomatoes, setPositiveTomatoe}){
+export default function Button ({addTomatoes, removeTomatoes}){
   return(
     <div className="buttons-container">
-            <button className="button" onClick={()=>setTomatoes(tomatoe=>tomatoe+1)}>
+            <button className="button" onClick={addTomatoes}>
                 Add 
             </button>
-            <button className="button" onClick={()=>setPositiveTomatoe()}>
+            <button className="button" onClick={removeTomatoes}>
                 Remove 
               </button>
           </div>
